4957bb5e245abf34ca8a45fa4757233b03c553c2
Your Name <you@example.com>
2025-08-04 20:49:35 +0330

    some changes

---
+++ api/server.go
@@ -0,0 +1,18 @@
+package api
+
+
+
+type Server struct {
+	store *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+
+	server.router = router
+	return server
+}
